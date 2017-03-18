f5888bd7be6b1c217db1106010be3e392ebc3550
wangfangning <285132709@qq.com>
2017-03-18 23:08:03 +0800

    压缩

--- src/main.js
+++ src/main.js
@@ -1,40 +1,6 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-import App from './App';
-
-Vue.use(VueRouter);
-Vue.use(VueResource);
-
-const Foo = {
-    template: '<div>foo</div>'
-};
-const Bar = {
-    template: '<div>bar</div>'
-};
-const Adc = {
-    template: '<div>adc</div>'
-};
-const routes = [
-    {
-        path: '/',
-        component: Foo
-    }, {
-        path: '/goods',
-        component: Foo
-    }, {
-        path: '/ratings',
-        component: Bar
-    }, {
-        path: '/seller',
-        component: Adc
-    }
-];
-
-const router = new VueRouter({linkActiveClass: 'active', routes});
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    router,
-    render: h => h(App)
-});
+import '../static/css/reset.css';
+import './style/app.scss';
+function hello (str) {
+  console.log('str');
+}
+hello('hello');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 //引入自动生成HTML文件插件
+var webpack = require('webpack');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 //nide.js 原生方法，不需要install
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('style/main.css');
 var utils = require('./build/utils.js');
 
 var env = process.env.NODE_ENV
@@ -27,40 +30,41 @@ module.exports = {
     output: {
         //输出文件路径
         path: './dist',
-        filename: 'js/[name].bundle.js'
+        filename: 'static/js/[name].bundle.js'
         //输出文件加上域名变成绝对路径
         // publicPath: 'http://www.163.com/'
     },
     resolve: {
-    extensions: ['', '.js', '.vue', '.json'],
-    fallback: [path.join(__dirname, '../node_modules')],
-    alias: {
-      'src': path.resolve(__dirname, '../src'),
-      'common': path.resolve(__dirname, '../src/common'),
-      'components': path.resolve(__dirname, '../src/components')
-    }
-  },
-  resolveLoader: {
-    fallback: [path.join(__dirname, '../node_modules')]
-  },
+        extensions: [
+            '', '.js', '.vue', '.json'
+        ],
+        fallback: [path.join(__dirname, '../node_modules')],
+        alias: {
+            'src': path.resolve(__dirname, '../src'),
+            'common': path.resolve(__dirname, '../src/common'),
+            'components': path.resolve(__dirname, '../src/components')
+        }
+    },
+    resolveLoader: {
+        fallback: [path.join(__dirname, '../node_modules')]
+    },
     //   resolve: { fallback: path.join(__dirname, "node_modules") },
     // resolveLoader: { fallback: path.join(__dirname, "node_modules") },
-    //   resolve: {
-    //   extensions: ['', '.js', '.vue', '.json'],
-    //   fallback: [path.join(__dirname, '../node_modules')],
-    //   alias: {
-    //     'src': path.resolve(__dirname, '../src'),
-    //     'common': path.resolve(__dirname, '../src/common'),
-    //     'components': path.resolve(__dirname, '../src/components')
-    //   }
-    // },
+      resolve: {
+      extensions: ['', '.js', '.vue', '.json'],
+      fallback: [path.join(__dirname, '../node_modules')],
+      alias: {
+        'src': path.resolve(__dirname, '../src'),
+        'common': path.resolve(__dirname, '../src/common'),
+        'components': path.resolve(__dirname, '../src/components')
+      }
+    },
     module: {
         loaders: [
-          {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
             {
+                test: /\.vue$/,
+                loader: 'vue'
+            }, {
                 //以.js结尾的文件 用babel-loader处理
                 test: /\.js$/,
                 //指定loader处理的文件,__dirname指当前运行环境路径，加上后面的参数路径，解析成绝对路径
@@ -74,10 +78,12 @@ module.exports = {
             }, {
                 test: /\.css$/,
                 //从右到左调用处理
-                loader: 'style!css?importLoaders=1!postcss'
+                loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
+                // 'style!css?importLoaders=1!postcss'
             }, {
                 test: /\.scss$/,
-                loader: 'style!css!postcss!sass'
+                loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
+                // 'style!css!postcss!sass'
             }, {
                 test: /\.less$/,
                 loader: 'style!css!postcss!less'
@@ -104,14 +110,22 @@ module.exports = {
     //     configFile: './.eslintrc.js'
     // },
     vue: {
-    loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['last 2 versions', 'Android >= 4.0']
-      })
-    ]
-  },
+        loaders: utils.cssLoaders({sourceMap: useCssSourceMap}),
+        postcss: [require('autoprefixer')({
+                browsers: ['last 2 versions', 'Android >= 4.0']
+            })]
+    },
     plugins: [
+      // extractCSS,
+         new ExtractTextPlugin(utils.assetsPath('css/[name].css'),{allChunks: false}),
+         //js,css压缩
+         new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        sourceMap: true,//这里的soucemap 不能少，可以在线上生成soucemap文件，便于调试
+        mangle: true
+    }),
         new htmlWebpackPlugin({
             filename: 'index.html',
             template: 'index.html',
